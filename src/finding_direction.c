2ddf685a01417608135a6536e11f7c384e9e426a
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2025-01-01 09:48:51 +0100

    adding player init from So_long project

--- src/finding_direction.c
+++ src/finding_direction.c
@@ -6,7 +6,7 @@
 /*   By: piotrwojnarowski <piotrwojnarowski@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 09:46:07 by piotrwojnar       #+#    #+#             */
-/*   Updated: 2025/01/01 09:46:10 by piotrwojnar      ###   ########.fr       */
+/*   Updated: 2025/01/01 09:48:25 by piotrwojnar      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,24 @@ t_position	find_element(char **map, char element, int height, int width)
 		row++;
 	}
 	return (pos);
+}
+
+void	initialize_player(t_config *config, t_map *map)
+{
+	t_position	pos;
+
+	pos = find_element(map->grid, 'N', map->height, map->width);
+	if (pos.row == -1 && pos.col == -1)
+		pos = find_element(map->grid, 'S', map->height, map->width);
+	if (pos.row == -1 && pos.col == -1)
+		pos = find_element(map->grid, 'E', map->height, map->width);
+	if (pos.row == -1 && pos.col == -1)
+		pos = find_element(map->grid, 'W', map->height, map->width);
+
+	if (pos.row == -1 && pos.col == -1)
+		ft_error(-7);
+	config->player.x = pos.col;
+	config->player.y = pos.row;
+	config->player.direction = map->grid[pos.row][pos.col];
+	map->grid[pos.row][pos.col] = '0';
 }
